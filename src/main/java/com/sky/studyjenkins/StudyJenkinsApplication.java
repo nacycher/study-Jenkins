c9e9a6cd270793c2b83ad697ebf567eb0c36ab0a
nacycher <nacycher@163.com>
2026-02-19 12:42:14 +0800

    init

---
+++ src/main/java/com/sky/studyjenkins/StudyJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.sky.studyjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sky/studyjenkins/StudyJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.studyjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
